e9796221cc2a15f143d1cc96fcc3f85dc04c2be6
Ricky McEvoy <rickymcevoy9@gmail.com>
2025-04-02 12:10:13 -0500

    The mysql table for the project.
    He wanted ids 1-10 but 20 entries confusingly enough. So I just made it to where the duplicate id entries are the student's 2nd set of test scores.

---
+++ 2project_students.sql
@@ -0,0 +1,52 @@
+-- MySQL dump 10.13  Distrib 8.0.41, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: 2project
+-- ------------------------------------------------------
+-- Server version	8.0.40
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `students`
+--
+
+DROP TABLE IF EXISTS `students`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `students` (
+  `id` int NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `score` int NOT NULL,
+  CONSTRAINT `students_chk_1` CHECK ((`score` between 0 and 100))
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `students`
+--
+
+LOCK TABLES `students` WRITE;
+/*!40000 ALTER TABLE `students` DISABLE KEYS */;
+INSERT INTO `students` VALUES (1,'John Michael Smith',95),(1,'John Michael Smith',78),(2,'Sarah Anne Johnson',32),(2,'Sarah Anne Johnson',88),(3,'David Lee Williams',65),(3,'David Lee Williams',47),(4,'Emma Grace Brown',76),(4,'Emma Grace Brown',91),(5,'James Robert Miller',54),(5,'James Robert Miller',39),(6,'Olivia Marie Davis',99),(6,'Olivia Marie Davis',23),(7,'Benjamin Lucas Wilson',12),(7,'Benjamin Lucas Wilson',84),(8,'Sophia Elizabeth Moore',100),(8,'Sophia Elizabeth Moore',7),(9,'Daniel Joseph Taylor',66),(9,'Daniel Joseph Taylor',29),(10,'Isabella Rose Anderson',83),(10,'Isabella Rose Anderson',5);
+/*!40000 ALTER TABLE `students` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-04-02 12:03:39
